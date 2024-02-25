fb037ae2d8c9bc8c7970a43315bc2a7671e402f0
Abhishek Bakshi <abhishek@finbox.in>
2024-02-26 03:36:50 +0530

    add: more pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -258,5 +258,30 @@
 				});
 
 			});
+	
+	// Menu.
+	var $menubig = $('#menubig'),
+		$menubig_openers = $menubig.children('ul').find('.opener');
+
+		// Openers.
+		$menubig_openers.each(function() {
+
+			var $this = $(this);
+
+			$this.on('click', function(event) {
+
+				// Prevent default.
+					event.preventDefault();
+
+				// Toggle.
+					$menubig_openers.not($this).removeClass('active');
+					$this.toggleClass('active');
+
+				// Trigger resize (sidebar lock).
+					$window.triggerHandler('resize.sidebar-lock');
+
+			});
+
+		});
 
 })(jQuery);
